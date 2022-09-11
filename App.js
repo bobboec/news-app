e4149ca1f72bf1834ae7827b7fb809269d7c6487
bobboec <30457666+bobboec@users.noreply.github.com>
2022-09-11 08:42:52 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Text</Text>
+      <Text>123324 4 </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +14,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
+
+
